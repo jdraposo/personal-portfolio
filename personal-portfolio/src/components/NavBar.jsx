d991bbbeaa8e6b17c55f9b7025f3e86347f6d017
jdraposo <jdcro@outlook.pt>
2023-07-09 23:25:02 +0100

    [feat] added react project, added NavBar component

---
+++ personal-portfolio/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {FaBars, FaTimes} from "react-icons/fa";
+
+const NavBar = () => {
+  return (
+    <div className="flex justify-between items-center w-full h-20 text-white fixed">
+      NavBar
+    </div>
+  )
+}
+
+export default NavBar
